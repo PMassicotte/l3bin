8cdc6481b4afb06c9ea6cb8f98979aab1449e4f2
Philippe Massicotte <pmassicotte@hotmail.com>
2025-05-22 09:00:38 -0400

    style: improve error messages readability using string interpolation syntax

--- src/errors.rs
+++ src/errors.rs
@@ -11,17 +11,13 @@ impl fmt::Display for IsinError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             IsinError::InvalidBinRange { max_bin } => {
-                write!(
-                    f,
-                    "Bin value is out of range. Maximum allowed is {}.",
-                    max_bin
-                )
+                write!(f, "Bin value must be between 1 and {max_bin}.")
             }
             IsinError::InvalidLatitude { min, max } => {
-                write!(f, "Latitude must be between {} and {}.", min, max)
+                write!(f, "Latitude must be between {min} and {max}.")
             }
             IsinError::InvalidLongitude { min, max } => {
-                write!(f, "Longitude must be between {} and {}.", min, max)
+                write!(f, "Longitude must be between {min} and {max}.")
             }
         }
     }
