c6adfe69102977f39374339e0d53e3e89f8f304b
Philippe Massicotte <pmassicotte@hotmail.com>
2025-05-22 09:06:44 -0400

    fix(errors): implement std::error::Error trait and fix string formatting syntax

--- src/errors.rs
+++ src/errors.rs
@@ -7,17 +7,19 @@ pub enum IsinError {
     InvalidLongitude { min: f64, max: f64 },
 }
 
+impl std::error::Error for IsinError {}
+
 impl fmt::Display for IsinError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             IsinError::InvalidBinRange { max_bin } => {
-                write!(f, "Bin value must be between 1 and {max_bin}.")
+                write!(f, "Bin value must be between 1 and {}.", max_bin)
             }
             IsinError::InvalidLatitude { min, max } => {
-                write!(f, "Latitude must be between {min} and {max}.")
+                write!(f, "Latitude must be between {} and {}.", min, max)
             }
             IsinError::InvalidLongitude { min, max } => {
-                write!(f, "Longitude must be between {min} and {max}.")
+                write!(f, "Longitude must be between {} and {}.", min, max)
             }
         }
     }
