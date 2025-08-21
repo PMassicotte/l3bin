79f78f8215400567918897674a58f58305d63868
Philippe Massicotte <pmassicotte@hotmail.com>
2025-08-21 14:15:28 -0400

    refactor(errors.rs): use Rust's format string syntax for error messages

--- src/errors.rs
+++ src/errors.rs
@@ -13,13 +13,13 @@ impl fmt::Display for IsinError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             IsinError::InvalidBinRange { max_bin } => {
-                write!(f, "Bin value must be between 1 and {}.", max_bin)
+                write!(f, "Bin value must be between 1 and {max_bin}.")
             }
             IsinError::InvalidLatitude { min, max } => {
-                write!(f, "Latitude must be between {} and {}.", min, max)
+                write!(f, "Latitude must be between {min} and {max}.")
             }
             IsinError::InvalidLongitude { min, max } => {
-                write!(f, "Longitude must be between {} and {}.", min, max)
+                write!(f, "Longitude must be between {min} and {max}.")
             }
         }
     }
